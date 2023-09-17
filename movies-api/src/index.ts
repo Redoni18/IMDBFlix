dda5586ecfb01df55a8c862c81254bd4ca6e9fe6
Redon Emini <redonemini18@gmail.com>
2023-09-18 00:54:08 +0200

    define get request routes

--- movies-api/src/index.ts
+++ movies-api/src/index.ts
@@ -1,6 +1,33 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .group('/search', {
+    query: t.Object({
+      q: t.String()
+    })
+  }, (app) => {
+    return app
+      .get('/search', ({ query }) => `query: ${query.q}`)
+      .get('/search/movie', ({ query }) => `query: ${query.q}`)
+      .get('/search/tv', ({ query }) => `query: ${query.q}`)
+      .get('/search/person', ({ query }) => `query: ${query.q}`)
+      .get('/search/company', ({ query }) => `query: ${query.q}`)
+      .get('/search/episode', ({ query }) => `query: ${query.q}`)
+      .get('/search/review', ({ query }) => `query: ${query.q}`)
+      .get('/search/award', ({ query }) => `query: ${query.q}`)
+  })
+  .group('/title/:id', {
+    params: t.Object({
+      id: t.Number()
+    })
+  }, (app) => {
+    return app
+      .get('/', ({params}) => `id: ${params.id}`)
+      .get('/episodes', ({params}) => `id: ${params.id}`)
+      .get('/cast', ({params}) => `id: ${params.id}`)
+      .get('/reviews', ({params}) => `id: ${params.id}`)
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
